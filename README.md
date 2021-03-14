375d746f2bfbd942f0dbb8b93ddba1e71437634f
Paul Miller <miller_p@hotmail.fr>
2021-03-14 10:59:35 +0100

    Added options for plotting with a more general implementation and an example for the readme

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 This is a set of functions to analyze the data from the data present directly on the [FOPH Covid-19 website](covid19.admin.ch), instead of using a github repo like my other [Covid-19 repo](https://github.com/paulbmiller/covid_19). This gives us more data to work with, such as the vaccination data.
 
 At this time, I've implemented some functions to check that the sums of some columns for cantons are consistent with values found for CH in the tables. I've also done some basic visualizations with matplotlib of full-vaccinations and a SARIMA model to forecast vaccinations.
+
+## Examples
+Examples of plots which can be drawn :
+![Multiple subplots](example.jpg)
--- main.py
+++ main.py
@@ -14,6 +14,7 @@ warnings.filterwarnings('ignore')
 
 ZIP_FILE_NAME = 'sources_csv.zip'
 CASES_GEOREGION = 'data/COVID19Cases_geoRegion.csv'
+DEATHS_GEOREGION = 'data/COVID19Death_geoRegion.csv'
 VACCDOSESADMIN = 'data/COVID19VaccDosesAdministered.csv'
 VACCDOSESDELIV = 'data/COVID19VaccDosesDelivered.csv'
 FULLYVACC = 'data/COVID19FullyVaccPersons.csv'
@@ -76,54 +77,86 @@ def check_sum_fullyvacc():
         print('Sum of cases for fully vaccinated are consistent.')
 
 
-def visualize(fn, column, plot_title, days=30, plot_herd_immunities=False):
+def visualize(fn, column, plot_title, plot_herd_immunities=False, agg_cantons=False, sarima_forecast=0, ax=None,
+              column_label=None, colors=['cadetblue', 'coral', 'lightpink', 'limegreen']):
+    """
+    Function to draw plots for a column.
+
+    :param fn: Name of the file which contains the column we want to plot
+    :param column: Name of the column to plot
+    :param plot_title: Title of the plot or subplot
+    :param plot_herd_immunities: Whether we want to plot the lines for herd immunities for vaccines
+    :param agg_cantons: Whether we want to use the aggregation of cantons (True) or the values at the CH index (False)
+    :param sarima_forecast: Number of days to forecast using a SARIMA model
+    :param ax: The axis to which matplotlib will draw the plot
+    :param column_label: The label we want to use for drawing the column
+    :param colors: Tuple of colors used
+    :return: None
+    """
     df = preprocess(fn)
-    CH_pop_total = df.loc['CH', :]['pop'].values[0]
 
-    df_cantons = df.drop('CH', axis=0, level='geoRegion')
-    df_cantons_sum = df_cantons[column].sum(axis=0, level='date')
+    if not column_label:
+        column_label = column
 
-    CH_vacc_df = df_cantons_sum.to_frame()
+    if plot_herd_immunities:
+        CH_pop_total = df.loc['CH', :]['pop'].values[0]
 
-    CH_vacc_df = CH_vacc_df[CH_vacc_df[column] > 0]
+    if agg_cantons:
+        df = df.drop('CH', axis=0, level='geoRegion')
+        df = df[column].sum(axis=0, level='date')
+        df = df.to_frame()
+    else:
+        df = df.loc['CH', :]
 
-    CH_vacc_df['percentTotal'] = CH_vacc_df[column] / CH_pop_total * 100
+    df = df[df[column] > 0]
 
-    # CH_vacc_df.plot(y='sumTotal', legend=True, title='Sum of fully vaccinated over total CH population')
-    # CH_vacc_df.plot(y='percentTotal', legend=True, title='Percentage of fully vaccinated over total CH population')
-    # CH_vacc_df.plot(y='logTotal', legend=True, title='Natural logarithm of the sum of fully vaccinated in CH')
-
-    # result = seasonal_decompose(CH_vacc_df['sumTotal'], model='add')
+    # result = seasonal_decompose(df[column], model='add')
     # result.plot()
 
-    stepwise_fit = auto_arima(CH_vacc_df[column], start_p=0, start_q=0, max_p=5, max_q=5, m=7, trace=True)
-    print(stepwise_fit.summary())
+    if sarima_forecast > 0:
+        stepwise_fit = auto_arima(df[column], start_p=0, start_q=0, max_p=5, max_q=5, m=7, trace=True)
+        print(stepwise_fit.summary())
 
-    model = SARIMAX(CH_vacc_df[column], order=stepwise_fit.order, seasonal_order=stepwise_fit.seasonal_order)
-    results = model.fit(disp=False)
-    # print(results.summary())
+        model = SARIMAX(df[column], order=stepwise_fit.order, seasonal_order=stepwise_fit.seasonal_order)
+        results = model.fit(disp=False)
+        # print(results.summary())
 
-    # Predict 30 days
-    predictions = results.predict(start=len(CH_vacc_df[column]), end=len(CH_vacc_df[column]) + days,
-                                  typ='levels').rename('SARIMA forecast')
+        # Predict ´sarima_forecast´ days
+        predictions = results.predict(start=len(df[column]), end=len(df[column]) + sarima_forecast,
+                                      typ='levels').rename('SARIMA forecast')
+        df = pd.concat([df, predictions], axis=1)
 
-    CH_vacc_df = pd.concat([CH_vacc_df, predictions], axis=1)
+    if not ax:
+        ax = plt.gca()
 
-    CH_vacc_df[[column, 'SARIMA forecast']].plot(
-        title=plot_title)
-    plt.gcf().axes[0].yaxis.get_major_formatter().set_scientific(False)
+    print(df)
+
+    ax.plot(df[column], label=column_label, color=colors[0])
+    if sarima_forecast > 0:
+        ax.plot(df['SARIMA forecast'], label='SARIMA forecast', color=colors[1])
+    ax.set_title(plot_title)
+
+    ax.yaxis.get_major_formatter().set_scientific(False)
     if plot_herd_immunities:
-        plt.axhline(y=CH_pop_total * 0.8, color='g', label='polio herd immunity')
-        plt.axhline(y=CH_pop_total * 0.95, color='c', label='measles herd immunity')
-    plt.legend()
-    plt.show()
+        ax.axhline(y=CH_pop_total * 0.8, color=colors[2], label='polio herd immunity')
+        ax.axhline(y=CH_pop_total * 0.95, color=colors[3], label='measles herd immunity')
+    ax.legend()
+
+    if not ax:
+        plt.show()
 
 
 if __name__ == '__main__':
     download_data()
     check_sum_cases()
     check_sum_fullyvacc()
-    # visualize_fully_vacc()
-    visualize(FULLYVACC, 'sumTotal', 'Forecasting of fully vaccinated people using a SARIMA model',
-              plot_herd_immunities=True)
-    visualize(VACCDOSESADMIN, 'sumTotal', 'Forecasting of administered vaccines using a SARIMA model')
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+    visualize(CASES_GEOREGION, 'entries', 'Switzerland Covid-19 confirmed cases', ax=ax1, column_label='confirmed',
+              colors=['cadetblue'])
+    visualize(DEATHS_GEOREGION, 'entries', 'Switzerland Covid-19 deaths', ax=ax2, column_label='deaths',
+              colors=['orangered'])
+    visualize(FULLYVACC, 'sumTotal', 'Forecasting of fully vaccinated people using a SARIMA model', agg_cantons=True,
+              plot_herd_immunities=True, sarima_forecast=30, ax=ax3)
+    visualize(VACCDOSESADMIN, 'sumTotal', 'Forecasting of administered vaccines using a SARIMA model',
+              sarima_forecast=30, agg_cantons=True, ax=ax4)
+    plt.show()
