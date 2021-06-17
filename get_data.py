0691b70bb936ab9709173294ec62adf97ae2bfa0
Paul Miller <miller_p@hotmail.fr>
2021-06-17 14:57:41 +0200

    fixed getting latest version

--- get_data.py
+++ get_data.py
@@ -1,16 +1,17 @@
 import pickle
-import datetime
 from urllib.request import urlopen
 import json
 
+
 def download_data():
-    new_available, version = new_data_available()
+    new_available, version, latest_url = new_data_available()
 
     if new_available:
-        with urlopen('https://www.covid19.admin.ch/api/data/20210310-kc5zw1wj/downloads/sources-csv.zip') as zip_file:
+        with urlopen(latest_url) as zip_file:
             with open('sources_csv.zip', 'wb') as out_file:
                 out_file.write(zip_file.read())
-                pickle.dump(version, open('data_version.p', 'wb'))
+                with open('data_version.p', 'wb') as version_file:
+                    pickle.dump(version, version_file)
                 print('Data successfully downloaded from the FOPH')
     else:
         print('We already have the latest version of the data')
@@ -20,14 +21,18 @@ def new_data_available():
     with urlopen('https://www.covid19.admin.ch/api/data/context') as f:
         data = json.load(f)
     latest_version = data['dataVersion']
+    latest_url = data['sources']['zip']['csv']
 
     try:
         my_version = pickle.load(open('data_version.p', 'rb'))
     except FileNotFoundError:
-        return (True, latest_version)
+        return True, latest_version, latest_url
 
     if my_version != latest_version:
-        return (True, latest_version)
+        return True, latest_version, latest_url
     else:
-        return (False, latest_version)
+        return False, latest_version, latest_url
 
+
+if __name__ == '__main__':
+    download_data()
